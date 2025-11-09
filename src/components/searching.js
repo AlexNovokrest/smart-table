57c5621661737617a9464a9d67b4cc7627c178a4
Alex Novo <Alexnovokrestx71@yandex.ru>
2025-11-09 15:19:54 +0500

    commit

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,16 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-
+    const comparator = createComparison(
+        rules.skipEmptyTargetValues, // стандартное правило для пропуска пустых значений
+        rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false) // поиск по нескольким полям
+    );
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
+                if (action && action.type === 'search' && action.payload !== undefined) {
+            const searchTerm = action.payload.toLowerCase();
+            return data.filter(item => comparator(item, searchTerm));
+        }
         return data;
     }
 }
